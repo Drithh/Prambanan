1cfb5690a7f8aa44a53693adfa7a36db8ebfd797
Drithh <adrielalfeus@gmail.com>
2021-05-15 21:47:44 +0700

    Timer

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    private float currentTime = 0f;
+    private float startingTime = 100f;
+    float milliseconds, seconds, minutes;
+
+    private void Start()
+    {
+        currentTime = startingTime;
+    }
+    private void Update()
+    {
+        currentTime -= Time.deltaTime;
+        milliseconds = ((float) currentTime % 1) * 100;
+        seconds = ((int) currentTime % 60);
+        minutes = (int) (currentTime / 60);
+        //gameObject.GetComponent<Text>().text = minutes.ToString("0").PadLeft(2, '0') + ":" + seconds.ToString("0").PadLeft(2, '0')  + ":" + milliseconds.ToString("0");
+        gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = minutes.ToString("0").PadLeft(2, '0') + ":" + seconds.ToString("0").PadLeft(2, '0')  + ":" + milliseconds.ToString("0");
+    }
+}
